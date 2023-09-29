c8cad3c0c86368ede5b12e2c329af929a298b353
Marcos Lozada Freytes <marcoslozadaf@hotmail.com>
2023-09-29 11:43:14 -0300

    holka

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,11 +5,13 @@ import Footer from "../components/Footer";
 import { appWithTranslation } from "next-i18next";
 import "../../i18n";
 import { metadata } from './metadata';
+import LogoBlack from "../components/LogoBlack";
 
 function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta image />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </head>
