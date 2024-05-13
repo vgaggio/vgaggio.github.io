c196413b3ce0409302c90398611ca7142b0a149b
Marcos Lozada Freytes <marcoslozadaf@hotmail.com>
2024-05-13 14:14:00 -0300

    Update layout.jsx
    ganalytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -18,19 +18,18 @@ const DOMAIN = "https://bircle.ai";
         <meta name="description" content={metadata.description} />
         <meta property="og:image" content={`${DOMAIN}/BircleAI.png`}></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
-        {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PYSWTG8KH5"></script>
-        <script
-            dangerouslySetInnerHTML={{
-                 __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'G-PYSWTG8KH5');
-                 `,
-              }}
-           />
       </head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-PYSWTG8KH5"></script>
+      <script
+          dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-PYSWTG8KH5');
+                `,
+            }}
+        />
       <body>
         <Header />
         {children}
