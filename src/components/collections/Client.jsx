322dc2ecff0559ef83654cb1e408d188dc02532a
SantiagoRivSal <santiagosalomon2001@gmail.com>
2024-05-22 12:22:31 -0300

    Cambios en el Diseño

--- src/components/collections/Client.jsx
+++ src/components/collections/Client.jsx
@@ -2,9 +2,12 @@
 import React, { useState, useEffect } from "react";
 import Container from "./Container";
 import Title from "./Title";
-import GestionPago1 from "../../../public/GestionDePagoEsp.svg";
-import GestionPago2 from "../../../public/GestionDePagoEng.svg";
+//import GestionPago1 from "../../../public/GestionDePagoEsp.svg";
+//import GestionPago2 from "../../../public/GestionDePagoEng.svg";
+import GestionPago1 from "../../../public/SinMarcosEsp.svg";
+import GestionPago2 from "../../../public/SinMarcosEng.svg";
 import Image from "next/image";
+import LineBackground from "./LineBackground"; // Importa el nuevo componente
 import { Gestion } from "../../constants";
 import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
@@ -32,11 +35,9 @@ const Client = () => {
 
     const settings = {
         dots: true,
-        className: "center",
-        centerMode: isMobile ? false : true,
         arrows: isMobile ? false : true, // Control de flechas basado en isMobile
         infinite: true,
-        slidesToShow: isMobile ? 1 : 2,
+        slidesToShow: isMobile ? 1 : 3,
         slidesToScroll: 1,
         adaptiveHeight: true,
         autoplay: true,  // Enable auto-play
@@ -55,15 +56,16 @@ const Client = () => {
                         title={t('Gestion de Deuda')}
                         className="text-4xl text-white text-center"
                     />
-                    <p className="mt-4 text-lg text-gray-300 text-center px-5">
+                    <div className="content-center flex items-center justify-content: center">
+                        <LineBackground color1="#CAF1B8" color2="#98C9F0" width={isMobile ? 500 : 576} height={isMobile ? 60 : 80}/> {/* Línea como separación */}
+                    </div>
+                    <p className=" text-lg text-gray-300 text-center px-5">
                         {t('Explicacion de la Gestion de Deuda')}
                     </p>
                 </div>
                 <div
                     className="flex items-center mx-auto max-w-3xl justify-center pt-4 rounded-3xl"
-                    style={{
-                      background: "linear-gradient(to right, #CAF1B8, #98C9F0)",
-                  }}
+
                 >
                     <div>
                         <Image src={imageToShow} className={`${isMobile ? "min-h-52 p-2" : "min-h-96 p-6"}`} alt="" layout="responsive" />
@@ -81,20 +83,20 @@ const Client = () => {
                         title={t('howItWorksTitleCol')}
                         className="text-3xl text-white pt-6 text-center"
                     />
-                    <Slider {...settings} className="mx-auto max-w-4xl text-left py-6">
+                    <Slider {...settings} className="mx-auto max-w-6xl text-left py-6">
                         {Gestion.map((item, index) => (
                             <div key={index} className="text-center py-2 px-6 ">
-                                <div className={`${isMobile ? "bg-gray-700 mx-auto max-w-xl sm:text-left rounded-xl h-56 py-2 px-2 content-center" : "bg-gray-700 mx-auto max-w-xl sm:text-left rounded-xl h-72 py-2 px-2 content-center"}`}>
+                                <div className={`${isMobile ? "bg-gray-700 mx-auto max-w-xl sm:text-left rounded-xl h-56 py-2 px-2 content-center" : "bg-gray-700 mx-auto max-w-xl sm:text-left rounded-xl h-72 py-2 px-2"}`}>
                                 <Image
-                                        className={`${isMobile ? "px-2 h-10 w-12 sm:h-24 sm:w-20" : "px-2 h-10 w-12 sm:h-20 sm:w-20"}`}
+                                        className={`${isMobile ? "px-2 h-10 w-12 sm:h-24 sm:w-20" : "px-2 h-10 w-10 sm:h-20 sm:w-20"}`}
                                         src={item.icon}
                                         alt=""
                                         height={30}
                                         width={30}
                                     />
-                                    <div className={`${isMobile ? "p-2 text-slate-200 font-bold text-left text-base" : "p-2 text-slate-200 font-bold text-left text-2xl"}`}>{t(item.name)}</div>
+                                    <div className={`${isMobile ? "p-2 text-slate-200 font-bold text-left text-base" : "p-2 text-slate-200 font-bold text-left text-xl"}`}>{t(item.name)}</div>
                                     <div className={`${isMobile ? "p-2 text-slate-200 font-bold text-left text-xs" : "p-4 text-slate-200 text-left text-sm"}`}>{t(item.text)}</div>
-                                </div>
+                                    </div>
                             </div>
                         ))}
                     </Slider>
