00e8691748217a6a9ca009760fd7e88079755c03
SantiagoRivSal <santiagosalomon2001@gmail.com>
2024-05-24 12:16:49 -0300

    Cambios en el Diseño

--- src/components/collections/Client.jsx
+++ src/components/collections/Client.jsx
@@ -2,12 +2,13 @@
 import React, { useState, useEffect } from "react";
 import Container from "./Container";
 import Title from "./Title";
-//import GestionPago1 from "../../../public/GestionDePagoEsp.svg";
-//import GestionPago2 from "../../../public/GestionDePagoEng.svg";
-import GestionPago1 from "../../../public/SinMarcosEsp.svg";
-import GestionPago2 from "../../../public/SinMarcosEng.svg";
+import Phone1Esp from "../../../public/Phone1Esp.svg";
+import Phone2Esp from "../../../public/Phone2Esp.svg";
+import Phone1Eng from "../../../public/Phone1Eng.svg";
+import Phone2Eng from "../../../public/Phone2Eng.svg";
+
 import Image from "next/image";
-import LineBackground from "./LineBackground"; // Importa el nuevo componente
+import LineBackground from "./LineBackground";
 import { Gestion } from "../../constants";
 import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
@@ -19,34 +20,43 @@ const Client = () => {
     const { t, i18n } = useTranslation();
 
     const [isMobile, setIsMobile] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
 
     useEffect(() => {
         const handleResize = () => {
             setIsMobile(window.innerWidth < 500);
         };
 
+        const handleScroll = () => {
+            const threshold = isMobile ? 610 : 300;
+            setIsVisible(window.scrollY > threshold);
+        };
+
         handleResize();
         window.addEventListener("resize", handleResize);
+        window.addEventListener("scroll", handleScroll);
 
         return () => {
             window.removeEventListener("resize", handleResize);
+            window.removeEventListener("scroll", handleScroll);
         };
-    }, []);
+    }, [isMobile]);
 
     const settings = {
         dots: true,
-        arrows: isMobile ? false : true, // Control de flechas basado en isMobile
+        arrows: isMobile ? false : true,
         infinite: true,
         slidesToShow: isMobile ? 1 : 3,
         slidesToScroll: 1,
         adaptiveHeight: true,
-        autoplay: true,  // Enable auto-play
-        autoplaySpeed: 5000,  //
-        pauseOnHover: true  // Pause auto-play on hover
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true
     };
 
     const currentLanguage = i18n.language;
-    const imageToShow = currentLanguage === 'en' ? GestionPago2 : GestionPago1;
+    const imageToShow1 = currentLanguage === 'en' ? Phone1Eng : Phone1Esp;
+    const imageToShow2 = currentLanguage === 'en' ? Phone2Eng : Phone2Esp;
 
     return (
         <section id="client" className={`bg-gray-900 ${isMobile ? "py-20" : "py-40"}`}>
@@ -59,35 +69,54 @@ const Client = () => {
                     <div className="content-center flex items-center justify-content: center">
                         <LineBackground color1="#CAF1B8" color2="#98C9F0" width={isMobile ? 500 : 576} height={isMobile ? 60 : 80}/> {/* Línea como separación */}
                     </div>
-                    <p className=" text-lg text-gray-300 text-center px-5">
+                    <p className="text-lg text-gray-300 text-center px-5">
                         {t('Explicacion de la Gestion de Deuda')}
                     </p>
                 </div>
-                <div
-                    className="flex items-center mx-auto max-w-3xl justify-center pt-4 rounded-3xl"
-
-                >
-                    <div>
-                        <Image src={imageToShow} className={`${isMobile ? "min-h-52 p-2" : "min-h-96 p-6"}`} alt="" layout="responsive" />
+                <div className="flex items-center mx-auto max-w-3xl justify-center rounded-3xl">
+                    <div className="flex relative">
+                        <Image
+                            src={imageToShow1}
+                            className={`transition-opacity duration-4000 ${isMobile ? "max-h-80 p-2" : "min-h-32 p-6"} ${isVisible ? "opacity-100 ease-in-out" : "opacity-0"}`}
+                            alt=""
+                            layout="responsive"
+                            style={{
+                                transform: isVisible
+                                    ? "translateY(0)"
+                                    : `translateY(-200px)`,
+                                transition: `opacity 4000ms ease-in-out, transform 2000ms ease-in-out`,
+                            }}
+                        />
+                        <Image
+                            src={imageToShow2}
+                            className={`transition-opacity duration-4000 ${isMobile ? "max-h-80 p-2" : "min-h-32 p-6"} ${isVisible ? "opacity-100 ease-in-out" : "opacity-0"}`}
+                            alt=""
+                            layout="responsive"
+                            style={{
+                                transform: isVisible
+                                    ? "translatey(0)"
+                                    : `translatey(200px)`,
+                                transition: `opacity 4000ms ease-in-out, transform 2000ms ease-in-out`,
+                            }}
+                        />
                     </div>
                 </div>
-                <div
-                    className="flex items-center mx-auto max-w-3xl justify-center p-6 pb-10 rounded-lg"
-                >
+
+                <div className="flex items-center mx-auto max-w-3xl justify-center p-6 pb-10 rounded-lg">
                     <p className={`${isMobile ? "text-lg text-center text-slate-200 px-5 flex-1" : "text-lg text-center text-slate-200 px-10 flex-1"}`}>
                         {t('Resuelve Tus Deudas con Facilidad y Tranquilidad')}
                     </p>
                 </div>
-                <div >
+                <div>
                     <Title
                         title={t('howItWorksTitleCol')}
                         className="text-3xl text-white pt-6 text-center"
                     />
                     <Slider {...settings} className="mx-auto max-w-6xl text-left py-6">
                         {Gestion.map((item, index) => (
-                            <div key={index} className="text-center py-2 px-6 ">
+                            <div key={index} className="text-center py-2 px-6">
                                 <div className={`${isMobile ? "bg-gray-700 mx-auto max-w-xl sm:text-left rounded-xl h-56 py-2 px-2 content-center" : "bg-gray-700 mx-auto max-w-xl sm:text-left rounded-xl h-72 py-2 px-2"}`}>
-                                <Image
+                                    <Image
                                         className={`${isMobile ? "px-2 h-10 w-12 sm:h-24 sm:w-20" : "px-2 h-10 w-10 sm:h-20 sm:w-20"}`}
                                         src={item.icon}
                                         alt=""
@@ -96,7 +125,7 @@ const Client = () => {
                                     />
                                     <div className={`${isMobile ? "p-2 text-slate-200 font-bold text-left text-base" : "p-2 text-slate-200 font-bold text-left text-xl"}`}>{t(item.name)}</div>
                                     <div className={`${isMobile ? "p-2 text-slate-200 font-bold text-left text-xs" : "p-4 text-slate-200 text-left text-sm"}`}>{t(item.text)}</div>
-                                    </div>
+                                </div>
                             </div>
                         ))}
                     </Slider>
@@ -107,4 +136,3 @@ const Client = () => {
 };
 
 export default Client;
-
--- src/components/collections/DobleCircleBackground.jsx
+++ src/components/collections/DobleCircleBackground.jsx
@@ -1,24 +1,31 @@
 import React, { useId } from "react"; // Importa React y el hook useId
 
-// Componente funcional que recibe props
-const BackgroundDesign = (props) => {
+// Componente funcional que recibe width, height, y otras props
+const DobleCircleBackground = ({ width, height, ...props }) => {
   let id = useId(); // Genera un ID único para el componente usando useId
   return (
     // Aplica todas las props al div contenedor
     <div {...props}>
       <svg
-        viewBox="0 0 1026 1026" // Define el tamaño del SVG
+        viewBox={`0 0 ${width} ${height}`} // Define el tamaño del SVG
         fill="none" // No rellena el SVG por defecto
         aria-hidden="true" // Oculta el SVG de tecnologías de accesibilidad
         className="absolute inset-0 h-full w-full animate-spin-slow" // Estilo del SVG, ocupando todo el contenedor y animándose lentamente
       >
         <path
-          d="M1025 513c0 282.77-229.23 512-512 512S1 795.77 1 513 230.23 1 513 1s512 229.23 512 512Z"
+          d={`M${width} ${height / 2}
+          c0 ${0.275 * height} -${0.225 * width} ${0.5 * height} -${0.5 * width} ${0.5 * height}
+          S1 ${0.775 * height} 1 ${height / 2}
+          ${0.225 * width} 1 ${width / 2} 1
+          s${0.5 * width} ${0.225 * height} ${0.5 * width} ${0.5 * height}
+          Z`}
           stroke="#D4D4D4" // Color del trazo
           strokeOpacity="0.7" // Opacidad del trazo
         />
         <path
-          d="M513 1025C230.23 1025 1 795.77 1 513"
+        
+          d={`  M${width / 2} ${height}
+          C${0.225 * width} ${height} 1 ${0.775 * height} 1 ${height / 2}`}
           stroke={`url(#${id}-gradient-1)`} // Aplica el gradiente usando el ID único
           strokeLinecap="round" // Define los extremos del trazo como redondeados
         />
@@ -27,9 +34,9 @@ const BackgroundDesign = (props) => {
           <linearGradient
             id={`${id}-gradient-1`} // ID único para el gradiente
             x1="1" // Coordenada x inicial del gradiente
-            y1="513" // Coordenada y inicial del gradiente
+            y1={height * 0.5} // Coordenada y inicial del gradiente
             x2="1" // Coordenada x final del gradiente
-            y2="1025" // Coordenada y final del gradiente
+            y2={height} // Coordenada y final del gradiente
             gradientUnits="userSpaceOnUse" // Unidades del gradiente
           >
             <stop stopColor="#06b6d4" /> 
@@ -38,18 +45,23 @@ const BackgroundDesign = (props) => {
         </defs>
       </svg>
       <svg
-        viewBox="0 0 1026 1026" // Define el tamaño del SVG
+        viewBox={`0 0 ${width} ${height}`} // Define el tamaño del SVG
         fill="none" // No rellena el SVG por defecto
         aria-hidden="true" // Oculta el SVG de tecnologías de accesibilidad
         className="absolute inset-0 h-full w-full animate-spin-reverse-slower" // Estilo del SVG, ocupando todo el contenedor y animándose en reversa lentamente
       >
         <path
-          d="M913 513c0 220.914-179.086 400-400 400S113 733.914 113 513s179.086-400 400-400 400 179.086 400 400Z"
+          d={` M${0.9 * width} ${height / 2}
+          c0 ${0.22 * height} -${0.18 * width} ${0.4 * height} -${0.4 * width} ${0.4 * height}
+          S${0.1 * width} ${0.72 * height} ${0.1 * width} ${height / 2}
+          s${0.18 * width} -${0.4 * height} ${0.4 * width} -${0.4 * height} ${0.4 * width} ${0.18 * height} ${0.4 * width} ${0.4 * height}
+          Z`}
           stroke="#D4D4D4" // Color del trazo
           strokeOpacity="0.7" // Opacidad del trazo
         />
         <path
-          d="M913 513c0 220.914-179.086 400-400 400"
+          d={` M${0.9 * width} ${height / 2}
+          c0 ${0.22 * height} -${0.18 * width} ${0.4 * height} -${0.4 * width} ${0.4 * height}`}
           stroke={`url(#${id}-gradient-2)`} // Aplica el gradiente usando el ID único
           strokeLinecap="round" // Define los extremos del trazo como redondeados
         />
@@ -57,10 +69,10 @@ const BackgroundDesign = (props) => {
           {/* Define un gradiente lineal */}
           <linearGradient
             id={`${id}-gradient-2`} // ID único para el gradiente
-            x1="913" // Coordenada x inicial del gradiente
-            y1="513" // Coordenada y inicial del gradiente
-            x2="913" // Coordenada x final del gradiente
-            y2="913" // Coordenada y final del gradiente
+            x1={width * 0.9} // Coordenada x inicial del gradiente
+            y1={height * 0.5} // Coordenada y inicial del gradiente
+            x2={width * 0.9} // Coordenada x final del gradiente
+            y2={height * 0.9} // Coordenada y final del gradiente
             gradientUnits="userSpaceOnUse" // Unidades del gradiente
           >
             <stop stopColor="#06b6d4" /> 
@@ -72,4 +84,5 @@ const BackgroundDesign = (props) => {
   );
 };
 
-export default BackgroundDesign; // Exporta el componente para su uso en otros archivos
+export default DobleCircleBackground; // Exporta el componente para su uso en otros archivos
+
